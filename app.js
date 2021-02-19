96bdfa4afcb0b63b78a68c8ad9bce4cfcc2d875f
FIRST_NAME LAST_NAME <sarwarishupadhyay@gmail.com>
2021-02-19 17:06:18 +0530

    make it better

--- app.js
+++ app.js
@@ -1,58 +1,66 @@
 const express = require('express');
 const keys = require('./config/keys');
-//const stripe = require('stripe')(keys.stripeSecretKey);
-const Stripe = require('stripe');
-const stripe = Stripe(keys.stripeSecretKey);
-
+const stripe = require('stripe')(keys.stripeSecretKey);
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
+
 const app = express();
 
-//Handlebars Middleware
+// Handlebars Middleware
 app.engine('handlebars',exphbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-//Set Static Folder
+// Set Static Folder
 app.use(express.static(`${__dirname}/public`));
 
-//Index Route
-app.get('/', (req,res) => {
-    res.render('index', {
-        stripePublishableKey : keys.stripePublishableKey
-    });
-});
-/*
-//Index Route
-app.get('/success', (req,res) => {
-    res.render('success');
-});
-*/
-//charge Route
-app.post('/charge', (req, res) => {
-    const amount =500;
-   
-    stripe.customers.create({
-        email: req.body.stripeEmail,
-        source: req.body.stripeToken
-    })
-    .then(customer => stripe.chargers.create({
-        amount,
-        description:"Donation for a good cause",
-        currency:'usd',
-        customer: customer.id
-    }))
-    .then(charge => res.render('success'));
+// Index Route
+app.get('/', (req, res) => {
+  res.render('index', {
+    stripePublishableKey: keys.stripePublishableKey
+  });
 });
 
-const port =process.env.PORT || 5000;
+// Charge Route
+app.post('/charge', (req, res) => {
+  const amount = 2500;
+  console.log(req.body);
+
+  return stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+    
+  })
+  .catch((error) => {
+    console.log('Promise errorB');
+  })
+  .then((customer) => {
+    console.log(customer);
+    stripe.charges.create({
+      amount:2500,
+      description: 'Web Development Ebook',
+      currency: 'USD',
+      customer: '1'
+    }
+      
+    );
+    console.log('here');
+  })
+  .then((charge) => {
+    console.log('Promise errorB');
+    res.render('success');
+  })
+  .catch((error) => {
+    console.log('Promise error');
+  });
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
-
-
